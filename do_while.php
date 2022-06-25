81f624aa1f9a97fcc7cb32bf3e45286499940dd1
Ubaidullah Bhutto <57267154+ubaidullah98@users.noreply.github.com>
2022-06-25 10:01:05 +0500

    Added some PHP files

---
+++ do_while.php
@@ -0,0 +1,12 @@
+<?php
+
+$i=1;
+do{
+    echo "this is number : $i"."<br>";
+    $i++;
+}while($i<=5);
+
+
+
+
+?>
---
+++ while_loop.php
@@ -0,0 +1,30 @@
+<?php
+
+$i=0;
+while($i<=10){
+    echo $i."<br>";
+    
+    $i++;
+}
+
+$j=0;
+while($j<=5){
+    echo "ubaid<br>";
+    $j++;
+}
+
+$x=0;
+while($x<=5){
+    if($x==5){
+        echo "the number is 5";
+    }
+    $x++;
+}
+
+
+
+
+
+
+
+?>
