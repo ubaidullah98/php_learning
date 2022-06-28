21905075d858523259203ee70b71b940b2eddb44
Ubaidullah Bhutto <57267154+ubaidullah98@users.noreply.github.com>
2022-06-28 11:31:56 +0500

    Added some new PHP files

--- arrays.php
+++ arrays.php
@@ -1,5 +1,7 @@
 <?php
+
 //------------------Indexed Array-------------//
+
 $arr[0] = "Ubaid";
 $arr[1] = "Azam";
 $arr[2] = "Basit";
@@ -17,16 +19,29 @@ foreach($arr1 as $x => $x_value){
     echo "<br>";
 }
 echo "<hr>";
+
 //-----------------Associative Array------------//
 
 $age = ["Ubaid"=>"23","Azam"=>"25","Basit"=>"22","Ahmad"=>  "22"];
 
-echo "Basit is ". $age["Basit"]." years old<br>";
+echo "Basit is ".$age["Basit"]." years old<br>";
 echo "Ahmad is ".$age["Ahmad"]." years old<br>";
 
 foreach($age as $y => $y_value){
     echo $y."\n =".$y_value."\n";
 }
+echo "<hr>";
+
+//-----------------Multiidimensional Arry-----------------//
+
+$multi = array(
+    array("Ubaid",60,70),
+    array("Azam",90,50),
+    array("Basit",80,60)
+
+);
+
+echo $multi[2][2];
 
 
 
